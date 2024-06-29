f33034aa03a78bece3d2a71dfba9ee56331b4e0b
Patrick Frankart <patrick.frankart@protonmail.com>
2024-06-29 09:27:01 -0400

    Initial commit.

---
+++ src/test/java/com/patrick/stockwatcher/StockwatcherApplicationTests.java
@@ -0,0 +1,14 @@
+package com.patrick.stockwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockwatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert true;
+	}
+
+}
